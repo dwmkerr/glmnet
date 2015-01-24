2502b44bd8fea6bdb2ac6db45724d4ba85e1b510
Dave Kerr <dwmkerr@gmail.com>
2015-01-24 18:45:32 +0530

    Removed incorrect namespaces, fixed version number

--- source/GlmNet/GlmNet/matrix.cs
+++ source/GlmNet/GlmNet/matrix.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Management.Instrumentation;
-using System.Text;
-
-namespace GlmNet
+﻿namespace GlmNet
 {
 // ReSharper disable InconsistentNaming
     public static partial class glm
