173271dee4801e6f211ef45424568e4bf9c5f975
Dave Kerr <dwmkerr@gmail.com>
2015-03-25 20:07:14 +0100

    Update README.md

--- source/GlmNet/GlmNet/mat4.cs
+++ source/GlmNet/GlmNet/mat4.cs
@@ -1,186 +1,233 @@
 using System;
 using System.Linq;
 
-namespace GlmNet 
+namespace GlmNet
 {
-    /// <summary>
-    /// Represents a 4x4 matrix.
-    /// </summary>
-	public struct mat4
-    {
-        #region Construction
+  /// <summary>
+  /// Represents a 4x4 matrix.
+  /// </summary>
+  public struct mat4
+  {
+    #region Construction
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="mat4"/> struct.
-        /// This matrix is the identity matrix scaled by <paramref name="scale"/>.
-        /// </summary>
-        /// <param name="scale">The scale.</param>
-		public mat4(float scale)
-        {
-            cols = new []
-            {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="mat4"/> struct.
+    /// This matrix is the identity matrix scaled by <paramref name="scale"/>.
+    /// </summary>
+    /// <param name="scale">The scale.</param>
+    public mat4(float scale)
+    {
+      cols = new[]
+      {
                 new vec4(scale, 0.0f, 0.0f, 0.0f),
                 new vec4(0.0f, scale, 0.0f, 0.0f),
                 new vec4(0.0f, 0.0f, scale, 0.0f),
                 new vec4(0.0f, 0.0f, 0.0f, scale),
             };
-        }
+    }
 
-        /// <summary>
-        /// Initializes a new instance of the <see cref="mat4"/> struct.
-        /// The matrix is initialised with the <paramref name="cols"/>.
-        /// </summary>
-        /// <param name="cols">The colums of the matrix.</param>
-        public mat4(vec4[] cols)
-        {
-            this.cols = new []
-            {
+    /// <summary>
+    /// Initializes a new instance of the <see cref="mat4"/> struct.
+    /// The matrix is initialised with the <paramref name="cols"/>.
+    /// </summary>
+    /// <param name="cols">The colums of the matrix.</param>
+    public mat4(vec4[] cols)
+    {
+      this.cols = new[]
+      {
                 cols[0],
                 cols[1],
                 cols[2],
                 cols[3]
             };
-        }
+    }
 
-        public mat4(vec4 a, vec4 b, vec4 c, vec4 d)
-        {
-            this.cols = new[]
-            {
+    public mat4(vec4 a, vec4 b, vec4 c, vec4 d)
+    {
+      this.cols = new[]
+      {
                 a, b, c, d
             };
-        }
+    }
 
-        /// <summary>
-        /// Creates an identity matrix.
-        /// </summary>
-        /// <returns>A new identity matrix.</returns>
-        public static mat4 identity()
-        {
-            return new mat4
-            {
-                cols = new[] 
-                {
+    /// <summary>
+    /// Creates an identity matrix.
+    /// </summary>
+    /// <returns>A new identity matrix.</returns>
+    public static mat4 identity()
+    {
+      return new mat4
+      {
+        cols = new[]
+          {
                     new vec4(1,0,0,0),
                     new vec4(0,1,0,0),
                     new vec4(0,0,1,0),
                     new vec4(0,0,0,1)
                 }
-            };
-        }
+      };
+    }
 
-        #endregion
+    #endregion
 
-        #region Index Access
+    #region Index Access
 
-        /// <summary>
-        /// Gets or sets the <see cref="vec4"/> column at the specified index.
-        /// </summary>
-        /// <value>
-        /// The <see cref="vec4"/> column.
-        /// </value>
-        /// <param name="column">The column index.</param>
-        /// <returns>The column at index <paramref name="column"/>.</returns>
-        public vec4 this[int column]
-		{
-            get { return cols[column]; }
-            set { cols[column] = value; }
-		}
+    /// <summary>
+    /// Gets or sets the <see cref="vec4"/> column at the specified index.
+    /// </summary>
+    /// <value>
+    /// The <see cref="vec4"/> column.
+    /// </value>
+    /// <param name="column">The column index.</param>
+    /// <returns>The column at index <paramref name="column"/>.</returns>
+    public vec4 this[int column]
+    {
+      get { return cols[column]; }
+      set { cols[column] = value; }
+    }
 
-        /// <summary>
-        /// Gets or sets the element at <paramref name="column"/> and <paramref name="row"/>.
-        /// </summary>
-        /// <value>
-        /// The element at <paramref name="column"/> and <paramref name="row"/>.
-        /// </value>
-        /// <param name="column">The column index.</param>
-        /// <param name="row">The row index.</param>
-        /// <returns>
-        /// The element at <paramref name="column"/> and <paramref name="row"/>.
-        /// </returns>
-        public float this[int column, int row]
-        {
-            get { return cols[column][row]; }
-            set { cols[column][row] = value; }
-        }
+    /// <summary>
+    /// Gets or sets the element at <paramref name="column"/> and <paramref name="row"/>.
+    /// </summary>
+    /// <value>
+    /// The element at <paramref name="column"/> and <paramref name="row"/>.
+    /// </value>
+    /// <param name="column">The column index.</param>
+    /// <param name="row">The row index.</param>
+    /// <returns>
+    /// The element at <paramref name="column"/> and <paramref name="row"/>.
+    /// </returns>
+    public float this[int column, int row]
+    {
+      get { return cols[column][row]; }
+      set { cols[column][row] = value; }
+    }
 
-        #endregion
+    #endregion
 
-        #region Conversion
-        
-        /// <summary>
-        /// Returns the matrix as a flat array of elements, column major.
-        /// </summary>
-        /// <returns></returns>
-        public float[] to_array()
-        {
-            return cols.SelectMany(v => v.to_array()).ToArray();
-        }
+    #region Conversion
 
-        /// <summary>
-        /// Returns the <see cref="mat3"/> portion of this matrix.
-        /// </summary>
-        /// <returns>The <see cref="mat3"/> portion of this matrix.</returns>
-        public mat3 to_mat3()
-        {
-            return new mat3(new[] {
-			new vec3(cols[0][0], cols[0][1], cols[0][2]),
-			new vec3(cols[1][0], cols[1][1], cols[1][2]),
-			new vec3(cols[2][0], cols[2][1], cols[2][2])});
-        }
+    /// <summary>
+    /// Returns the matrix as a flat array of elements, column major.
+    /// </summary>
+    /// <returns></returns>
+    public float[] to_array()
+    {
+      return cols.SelectMany(v => v.to_array()).ToArray();
+    }
 
-        #endregion
+    /// <summary>
+    /// Returns the <see cref="mat3"/> portion of this matrix.
+    /// </summary>
+    /// <returns>The <see cref="mat3"/> portion of this matrix.</returns>
+    public mat3 to_mat3()
+    {
+      return new mat3(new[] {
+      new vec3(cols[0][0], cols[0][1], cols[0][2]),
+      new vec3(cols[1][0], cols[1][1], cols[1][2]),
+      new vec3(cols[2][0], cols[2][1], cols[2][2])});
+    }
 
-        #region Multiplication
+    #endregion
 
-        /// <summary>
-        /// Multiplies the <paramref name="lhs"/> matrix by the <paramref name="rhs"/> vector.
-        /// </summary>
-        /// <param name="lhs">The LHS matrix.</param>
-        /// <param name="rhs">The RHS vector.</param>
-        /// <returns>The product of <paramref name="lhs"/> and <paramref name="rhs"/>.</returns>
-        public static vec4 operator *(mat4 lhs, vec4 rhs)
-        {
-            return new vec4(
-                lhs[0, 0] * rhs[0] + lhs[1, 0] * rhs[1] + lhs[2, 0] * rhs[2] + lhs[3, 0] * rhs[3],
-                lhs[0, 1] * rhs[0] + lhs[1, 1] * rhs[1] + lhs[2, 1] * rhs[2] + lhs[3, 1] * rhs[3],
-                lhs[0, 2] * rhs[0] + lhs[1, 2] * rhs[1] + lhs[2, 2] * rhs[2] + lhs[3, 2] * rhs[3],
-                lhs[0, 3] * rhs[0] + lhs[1, 3] * rhs[1] + lhs[2, 3] * rhs[2] + lhs[3, 3] * rhs[3]
-            );
-        }
+    #region Multiplication
 
-        /// <summary>
-        /// Multiplies the <paramref name="lhs"/> matrix by the <paramref name="rhs"/> matrix.
-        /// </summary>
-        /// <param name="lhs">The LHS matrix.</param>
-        /// <param name="rhs">The RHS matrix.</param>
-        /// <returns>The product of <paramref name="lhs"/> and <paramref name="rhs"/>.</returns>
-        public static mat4 operator * (mat4 lhs, mat4 rhs)
-        {
-            return new mat4(new []
-            {
-			    lhs[0][0] * rhs[0] + lhs[1][0] * rhs[1] + lhs[2][0] * rhs[2] + lhs[3][0] * rhs[3],
-			    lhs[0][1] * rhs[0] + lhs[1][1] * rhs[1] + lhs[2][1] * rhs[2] + lhs[3][1] * rhs[3],
-			    lhs[0][2] * rhs[0] + lhs[1][2] * rhs[1] + lhs[2][2] * rhs[2] + lhs[3][2] * rhs[3],
-			    lhs[0][3] * rhs[0] + lhs[1][3] * rhs[1] + lhs[2][3] * rhs[2] + lhs[3][3] * rhs[3]
+    /// <summary>
+    /// Multiplies the <paramref name="lhs"/> matrix by the <paramref name="rhs"/> vector.
+    /// </summary>
+    /// <param name="lhs">The LHS matrix.</param>
+    /// <param name="rhs">The RHS vector.</param>
+    /// <returns>The product of <paramref name="lhs"/> and <paramref name="rhs"/>.</returns>
+    public static vec4 operator *(mat4 lhs, vec4 rhs)
+    {
+      return new vec4(
+          lhs[0, 0] * rhs[0] + lhs[1, 0] * rhs[1] + lhs[2, 0] * rhs[2] + lhs[3, 0] * rhs[3],
+          lhs[0, 1] * rhs[0] + lhs[1, 1] * rhs[1] + lhs[2, 1] * rhs[2] + lhs[3, 1] * rhs[3],
+          lhs[0, 2] * rhs[0] + lhs[1, 2] * rhs[1] + lhs[2, 2] * rhs[2] + lhs[3, 2] * rhs[3],
+          lhs[0, 3] * rhs[0] + lhs[1, 3] * rhs[1] + lhs[2, 3] * rhs[2] + lhs[3, 3] * rhs[3]
+      );
+    }
+
+    /// <summary>
+    /// Multiplies the <paramref name="lhs"/> matrix by the <paramref name="rhs"/> matrix.
+    /// </summary>
+    /// <param name="lhs">The LHS matrix.</param>
+    /// <param name="rhs">The RHS matrix.</param>
+    /// <returns>The product of <paramref name="lhs"/> and <paramref name="rhs"/>.</returns>
+    public static mat4 operator *(mat4 lhs, mat4 rhs)
+    {
+      return new mat4(new[]
+      {
+          lhs[0][0] * rhs[0] + lhs[1][0] * rhs[1] + lhs[2][0] * rhs[2] + lhs[3][0] * rhs[3],
+          lhs[0][1] * rhs[0] + lhs[1][1] * rhs[1] + lhs[2][1] * rhs[2] + lhs[3][1] * rhs[3],
+          lhs[0][2] * rhs[0] + lhs[1][2] * rhs[1] + lhs[2][2] * rhs[2] + lhs[3][2] * rhs[3],
+          lhs[0][3] * rhs[0] + lhs[1][3] * rhs[1] + lhs[2][3] * rhs[2] + lhs[3][3] * rhs[3]
             });
-        }
+    }
 
-        public static mat4 operator *(mat4 lhs, float s)
-        {
-            return new mat4(new[]
-            {
+    public static mat4 operator *(mat4 lhs, float s)
+    {
+      return new mat4(new[]
+      {
                 lhs[0]*s,
                 lhs[1]*s,
                 lhs[2]*s,
                 lhs[3]*s
             });
-        }
+    }
 
-        #endregion
+    #endregion
 
-        /// <summary>
-        /// The columms of the matrix.
-        /// </summary>
-        private vec4[] cols;
-	}
+    #region comparision
+    /// <summary>
+    /// Determines whether the specified <see cref="System.Object" />, is equal to this instance.
+    /// The Difference is detected by the different values
+    /// </summary>
+    /// <param name="obj">The <see cref="System.Object" /> to compare with this instance.</param>
+    /// <returns>
+    ///   <c>true</c> if the specified <see cref="System.Object" /> is equal to this instance; otherwise, <c>false</c>.
+    /// </returns>
+    public override bool Equals(object obj)
+    {
+      if (obj.GetType() == typeof(mat4))
+      {
+        var mat = (mat4)obj;
+        if (mat[0] == this[0] && mat[1] == this[1] && mat[2] == this[2] && mat[3] == this[3])
+          return true;
+      }
+
+      return false;
+    }
+    /// <summary>
+    /// Implements the operator ==.
+    /// </summary>
+    /// <param name="m1">The first Matrix.</param>
+    /// <param name="m2">The second Matrix.</param>
+    /// <returns>
+    /// The result of the operator.
+    /// </returns>
+    public static bool operator ==(mat4 m1, mat4 m2)
+    {
+      return m1.Equals(m2);
+    }
+
+    /// <summary>
+    /// Implements the operator !=.
+    /// </summary>
+    /// <param name="m1">The first Matrix.</param>
+    /// <param name="m2">The second Matrix.</param>
+    /// <returns>
+    /// The result of the operator.
+    /// </returns>
+    public static bool operator !=(mat4 m1, mat4 m2)
+    {
+      return !m1.Equals(m2);
+    }
+    #endregion
+
+    /// <summary>
+    /// The columms of the matrix.
+    /// </summary>
+    private vec4[] cols;
+  }
 }
--- source/GlmNet/GlmNet/vec3.cs
+++ source/GlmNet/GlmNet/vec3.cs
@@ -2,108 +2,155 @@ using System;
 
 namespace GlmNet
 {
+  /// <summary>
+  /// Represents a three dimensional vector.
+  /// </summary>
+  public struct vec3
+  {
+    public float x;
+    public float y;
+    public float z;
+
+    public float this[int index]
+    {
+      get
+      {
+        if (index == 0) return x;
+        else if (index == 1) return y;
+        else if (index == 2) return z;
+        else throw new Exception("Out of range.");
+      }
+      set
+      {
+        if (index == 0) x = value;
+        else if (index == 1) y = value;
+        else if (index == 2) z = value;
+        else throw new Exception("Out of range.");
+      }
+    }
+
+    public vec3(float s)
+    {
+      x = y = z = s;
+    }
+
+    public vec3(float x, float y, float z)
+    {
+      this.x = x;
+      this.y = y;
+      this.z = z;
+    }
+
+    public vec3(vec3 v)
+    {
+      this.x = v.x;
+      this.y = v.y;
+      this.z = v.z;
+    }
+
+    public vec3(vec4 v)
+    {
+      this.x = v.x;
+      this.y = v.y;
+      this.z = v.z;
+    }
+
+    public vec3(vec2 xy, float z)
+    {
+      this.x = xy.x;
+      this.y = xy.y;
+      this.z = z;
+    }
+
+    public static vec3 operator +(vec3 lhs, vec3 rhs)
+    {
+      return new vec3(lhs.x + rhs.x, lhs.y + rhs.y, lhs.z + rhs.z);
+    }
+
+    public static vec3 operator +(vec3 lhs, float rhs)
+    {
+      return new vec3(lhs.x + rhs, lhs.y + rhs, lhs.z + rhs);
+    }
+
+    public static vec3 operator -(vec3 lhs, vec3 rhs)
+    {
+      return new vec3(lhs.x - rhs.x, lhs.y - rhs.y, lhs.z - rhs.z);
+    }
+
+    public static vec3 operator -(vec3 lhs, float rhs)
+    {
+      return new vec3(lhs.x - rhs, lhs.y - rhs, lhs.z - rhs);
+    }
+
+    public static vec3 operator *(vec3 self, float s)
+    {
+      return new vec3(self.x * s, self.y * s, self.z * s);
+    }
+    public static vec3 operator *(float lhs, vec3 rhs)
+    {
+      return new vec3(rhs.x * lhs, rhs.y * lhs, rhs.z * lhs);
+    }
+
+    public static vec3 operator /(vec3 lhs, float rhs)
+    {
+      return new vec3(lhs.x / rhs, lhs.y / rhs, lhs.z / rhs);
+    }
+
+    public static vec3 operator *(vec3 lhs, vec3 rhs)
+    {
+      return new vec3(rhs.x * lhs.x, rhs.y * lhs.y, rhs.z * lhs.z);
+    }
+
+    public float[] to_array()
+    {
+      return new[] { x, y, z };
+    }
+
+    #region comparision
     /// <summary>
-    /// Represents a three dimensional vector.
+    /// Determines whether the specified <see cref="System.Object" />, is equal to this instance.
+    /// The Difference is detected by the different values
     /// </summary>
-	public struct vec3
-	{
-		public float x;
-		public float y;
-		public float z;
+    /// <param name="obj">The <see cref="System.Object" /> to compare with this instance.</param>
+    /// <returns>
+    ///   <c>true</c> if the specified <see cref="System.Object" /> is equal to this instance; otherwise, <c>false</c>.
+    /// </returns>
+    public override bool Equals(object obj)
+    {
+      if (obj.GetType() == typeof(vec3))
+      {
+        var vec = (vec3)obj;
+        if (this.x == vec.x && this.y == vec.y && this.z == vec.z)
+          return true;
+      }
 
-		public float this[int index]
-		{
-			get 
-			{
-				if(index == 0) return x;
-				else if(index == 1) return y;
-				else if(index == 2) return z;
-                else throw new Exception("Out of range.");
-			}
-			set 
-			{
-				if(index == 0) x = value;
-                else if (index == 1) y = value;
-                else if (index == 2) z = value;
-                else throw new Exception("Out of range.");
-			}
-		}
+      return false;
+    }
+    /// <summary>
+    /// Implements the operator ==.
+    /// </summary>
+    /// <param name="v1">The first Vector.</param>
+    /// <param name="v2">The second Vector.</param>
+    /// <returns>
+    /// The result of the operator.
+    /// </returns>
+    public static bool operator ==(vec3 v1, vec3 v2)
+    {
+      return v1.Equals(v2);
+    }
 
-		public vec3(float s)
-		{
-			x = y = z = s;
-		}
-
-		public vec3(float x, float y, float z)
-		{
-			this.x = x;
-			this.y = y;
-            this.z = z;
-		}
-
-        public vec3(vec3 v)
-        {
-            this.x = v.x;
-            this.y = v.y;
-            this.z = v.z;
-        }
-
-        public vec3(vec4 v)
-        {
-            this.x = v.x;
-            this.y = v.y;
-            this.z = v.z; 
-        }
-
-        public vec3(vec2 xy, float z)
-        {
-            this.x = xy.x;
-            this.y = xy.y;
-            this.z = z;
-        }
-		
-		public static vec3 operator + (vec3 lhs, vec3 rhs)
-		{
-			return new vec3(lhs.x + rhs.x, lhs.y + rhs.y, lhs.z + rhs.z);
-		}
-
-        public static vec3 operator +(vec3 lhs, float rhs)
-        {
-            return new vec3(lhs.x + rhs, lhs.y + rhs, lhs.z + rhs);
-        }
-
-        public static vec3 operator -(vec3 lhs, vec3 rhs)
-        {
-            return new vec3(lhs.x - rhs.x, lhs.y - rhs.y, lhs.z - rhs.z);
-        }
-
-        public static vec3 operator - (vec3 lhs, float rhs)
-        {
-            return new vec3(lhs.x - rhs, lhs.y - rhs, lhs.z - rhs);
-        }
-
-        public static vec3 operator *(vec3 self, float s)
-		{
-			return new vec3(self.x * s, self.y * s, self.z * s);
-		}
-        public static vec3 operator *(float lhs, vec3 rhs)
-        {
-            return new vec3(rhs.x * lhs, rhs.y * lhs, rhs.z * lhs);
-        }
-
-        public static vec3 operator /(vec3 lhs, float rhs)
-        {
-            return new vec3(lhs.x / rhs, lhs.y / rhs, lhs.z / rhs);
-        }
-
-        public static vec3 operator * (vec3 lhs, vec3 rhs)
-        {
-            return new vec3(rhs.x * lhs.x, rhs.y * lhs.y, rhs.z * lhs.z);
-        }
-
-        public float[] to_array()
-        {
-            return new[] { x, y, z };
-        }
-	}
+    /// <summary>
+    /// Implements the operator !=.
+    /// </summary>
+    /// <param name="v1">The first Vector.</param>
+    /// <param name="v2">The second Vector.</param>
+    /// <returns>
+    /// The result of the operator.
+    /// </returns>
+    public static bool operator !=(vec3 v1, vec3 v2)
+    {
+      return !v1.Equals(v2);
+    }
+    #endregion
+  }
 }
