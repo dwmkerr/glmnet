7aa1933e9721e803e2dd537c4c353436f00613d1
Dave Kerr <dwmkerr@gmail.com>
2019-04-26 21:11:20 +0800

    fix: fix matrix multiplication not being associative

--- source/GlmNet/GlmNet/mat4.cs
+++ source/GlmNet/GlmNet/mat4.cs
@@ -158,10 +158,10 @@ namespace GlmNet
         {
             return new mat4(new []
             {
-			    lhs[0][0] * rhs[0] + lhs[1][0] * rhs[1] + lhs[2][0] * rhs[2] + lhs[3][0] * rhs[3],
-			    lhs[0][1] * rhs[0] + lhs[1][1] * rhs[1] + lhs[2][1] * rhs[2] + lhs[3][1] * rhs[3],
-			    lhs[0][2] * rhs[0] + lhs[1][2] * rhs[1] + lhs[2][2] * rhs[2] + lhs[3][2] * rhs[3],
-			    lhs[0][3] * rhs[0] + lhs[1][3] * rhs[1] + lhs[2][3] * rhs[2] + lhs[3][3] * rhs[3]
+                rhs[0][0] * lhs[0] + rhs[0][1] * lhs[1] + rhs[0][2] * lhs[2] + rhs[0][3] * lhs[3],
+                rhs[1][0] * lhs[0] + rhs[1][1] * lhs[1] + rhs[1][2] * lhs[2] + rhs[1][3] * lhs[3],
+                rhs[2][0] * lhs[0] + rhs[2][1] * lhs[1] + rhs[2][2] * lhs[2] + rhs[2][3] * lhs[3],
+                rhs[3][0] * lhs[0] + rhs[3][1] * lhs[1] + rhs[3][2] * lhs[2] + rhs[3][3] * lhs[3]
             });
         }
 
--- source/GlmNet/Tests/mat4Tests.cs
+++ source/GlmNet/Tests/mat4Tests.cs
@@ -79,5 +79,79 @@ namespace Tests
 
             Assert.AreEqual(new vec4(538f, 612f, 686f, 760f), m * v, "Rank 2 Matrix * Vector results are incorrect.");
         }
+
+        [Test]
+        public void IdentityIsCorrect()
+        {
+            var identity1 = mat4.identity();
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    if (i == j) Assert.AreEqual(1f, identity1[i][j], "Diagonal elements not 1.0f");
+                    else Assert.AreEqual(0f, identity1[i][j], "Off-diagonal elements not 0.0f");
+                }
+            }
+
+            var identity2 = new mat4(1f);
+            for (int i = 0; i < 4; i++)
+            {
+                for (int j = 0; j < 4; j++)
+                {
+                    if (i == j) Assert.AreEqual(1f, identity2[i][j], "Diagonal elements not 1.0f");
+                    else Assert.AreEqual(0f, identity2[i][j], "Off-diagonal elements not 0.0f");
+                }
+            }
+        }
+
+        [Test]
+        public void IdentityMultiplicationLeft()
+        {
+            var identity = mat4.identity();
+
+            var m = new mat4(new[] {
+                new vec4(1f, 2f, 3f, 4f),
+                new vec4(5f, 6f, 7f, 8f),
+                new vec4(9f, 10f, 11f, 12f),
+                new vec4(13f, 14f, 15f, 16f)
+            });
+
+            Assert.AreEqual(new vec4(1f, 2f, 3f, 4f), m[0], "Col 1 wrong");
+            Assert.AreEqual(new vec4(5f, 6f, 7f, 8f), m[1], "Col 2 wrong");
+            Assert.AreEqual(new vec4(9f, 10f, 11f, 12f), m[2], "Col 3 wrong");
+            Assert.AreEqual(new vec4(13f, 14f, 15f, 16f), m[3], "Col 4 wrong");
+
+            var m1 = m * identity;
+
+            Assert.AreEqual(new vec4(1f, 2f, 3f, 4f), m1[0], "Col 1 wrong post multiply m*i");
+            Assert.AreEqual(new vec4(5f, 6f, 7f, 8f), m1[1], "Col 2 wrong post multiply m*i");
+            Assert.AreEqual(new vec4(9f, 10f, 11f, 12f), m1[2], "Col 3 wrong post multiply m*i");
+            Assert.AreEqual(new vec4(13f, 14f, 15f, 16f), m1[3], "Col 4 wrong post multiply m*i");
+        }
+
+        [Test]
+        public void IdentityMultiplicationRight()
+        {
+            var identity = mat4.identity();
+
+            var m = new mat4(new[] {
+                new vec4(1f, 2f, 3f, 4f),
+                new vec4(5f, 6f, 7f, 8f),
+                new vec4(9f, 10f, 11f, 12f),
+                new vec4(13f, 14f, 15f, 16f)
+            });
+
+            Assert.AreEqual(new vec4(1f, 2f, 3f, 4f), m[0], "Col 1 wrong");
+            Assert.AreEqual(new vec4(5f, 6f, 7f, 8f), m[1], "Col 2 wrong");
+            Assert.AreEqual(new vec4(9f, 10f, 11f, 12f), m[2], "Col 3 wrong");
+            Assert.AreEqual(new vec4(13f, 14f, 15f, 16f), m[3], "Col 4 wrong");
+
+            var m2 = identity * m;
+
+            Assert.AreEqual(new vec4(1f, 2f, 3f, 4f), m2[0], "Col 1 wrong post multiply i*m");
+            Assert.AreEqual(new vec4(5f, 6f, 7f, 8f), m2[1], "Col 2 wrong post multiply i*m");
+            Assert.AreEqual(new vec4(9f, 10f, 11f, 12f), m2[2], "Col 3 wrong post multiply i*m");
+            Assert.AreEqual(new vec4(13f, 14f, 15f, 16f), m2[3], "Col 4 wrong post multiply i*m");
+        }
     }
 }
