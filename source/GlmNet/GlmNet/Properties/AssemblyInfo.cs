3f25e8975e1eee10a05c8fe5eda587c2348c52a9
Ansgar Hellwig <code@ahellwig.eu>
2020-03-18 12:19:19 +0100

    Update solution to VS 2019 and project file to SDK style.

--- source/GlmNet/GlmNet/Properties/AssemblyInfo.cs
+++ source/GlmNet/GlmNet/Properties/AssemblyInfo.cs
@@ -1,18 +1,4 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
-using System.Runtime.InteropServices;
-
-// General Information about an assembly is controlled through the following 
-// set of attributes. Change these attribute values to modify the information
-// associated with an assembly.
-[assembly: AssemblyTitle("GlmNet")]
-[assembly: AssemblyDescription("GlmNet is a .NET version of the popular GLM mathematics library.")]
-[assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("GlmNet")]
-[assembly: AssemblyCopyright("Copyright © Dave Kerr 2019")]
-[assembly: AssemblyTrademark("")]
-[assembly: AssemblyCulture("")]
+﻿using System.Runtime.InteropServices;
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
@@ -21,16 +7,3 @@ using System.Runtime.InteropServices;
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("4d6cdf78-36f5-40ee-bcc2-4e79e34b24be")]
-
-// Version information for an assembly consists of the following four values:
-//
-//      Major Version
-//      Minor Version 
-//      Build Number
-//      Revision
-//
-// You can specify all the values or you can default the Build and Revision Numbers 
-// by using the '*' as shown below:
-// [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.7.0.0")]
-[assembly: AssemblyFileVersion("0.7.0.0")]
